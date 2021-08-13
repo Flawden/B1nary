51059fbac36ecbf9c2baccd2cbb37c9a2348e311
Flawden <43092499+Flawden@users.noreply.github.com>
2021-08-13 18:37:50 +0300

    First commit
    First commit

---
+++ js/Script.js
@@ -0,0 +1,76 @@
+
+window.addEventListener('load', function() {
+  baguetteBox.run('.gallery');
+});
+
+
+$('a[href^="#"]').bind('click.smoothscroll',function (e) {
+ e.preventDefault();
+ 
+var target = this.hash,
+ $target = $(target);
+ 
+$('html, body').stop().animate({
+ 'scrollTop': $target.offset().top
+ }, 500, 'swing', function () {
+ window.location.hash = target;
+ });
+ });
+
+function ibg(){
+
+$.each($('.ibg'), function(index, val) {
+if($(this).find('img').length>0){
+$(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
+}
+});
+}
+
+ibg();
+
+$(window).resize(function(event) {
+
+
+
+});
+
+function mainblock() {
+
+	let h = $(window).outerHeight();
+
+		$('.page').css('min-height', h);
+
+}
+
+$('.filter__item').click(function(event) {
+
+	let i=$(this).data('filter');
+	
+		if(i == 1) {
+
+			$('.portfolio__column').show();
+
+		}
+
+		else {
+
+			$('.portfolio__column').hide();
+			$('.portfolio__column.f_'+i).show();
+
+		}
+
+	$('.filter__item').removeClass('filter__item_active');
+	$(this).addClass('filter__item_active');
+
+	return false;
+
+});
+
+/*
+CSS Browser Selector v0.4.0 (Nov 02, 2010)
+Rafael Lima (http://rafael.adm.br)
+http://rafael.adm.br/css_browser_selector
+License: http://creativecommons.org/licenses/by/2.5/
+Contributors: http://rafael.adm.br/css_browser_selector#contributors
+*/
+function css_browser_selector(u){var ua=u.toLowerCase(),is=function(t){return ua.indexOf(t)>-1},g='gecko',w='webkit',s='safari',o='opera',m='mobile',h=document.documentElement,b=[(!(/opera|webtv/i.test(ua))&&/msie\s(\d)/.test(ua))?('ie ie'+RegExp.$1):is('firefox/2')?g+' ff2':is('firefox/3.5')?g+' ff3 ff3_5':is('firefox/3.6')?g+' ff3 ff3_6':is('firefox/3')?g+' ff3':is('gecko/')?g:is('opera')?o+(/version\/(\d+)/.test(ua)?' '+o+RegExp.$1:(/opera(\s|\/)(\d+)/.test(ua)?' '+o+RegExp.$2:'')):is('konqueror')?'konqueror':is('blackberry')?m+' blackberry':is('android')?m+' android':is('chrome')?w+' chrome':is('iron')?w+' iron':is('applewebkit/')?w+' '+s+(/version\/(\d+)/.test(ua)?' '+s+RegExp.$1:''):is('mozilla/')?g:'',is('j2me')?m+' j2me':is('iphone')?m+' iphone':is('ipod')?m+' ipod':is('ipad')?m+' ipad':is('mac')?'mac':is('darwin')?'mac':is('webtv')?'webtv':is('win')?'win'+(is('windows nt 6.0')?' vista':''):is('freebsd')?'freebsd':(is('x11')||is('linux'))?'linux':'','js']; c = b.join(' '); h.className += ' '+c; return c;}; css_browser_selector(navigator.userAgent);
